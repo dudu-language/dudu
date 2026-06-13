f65ac890394e5ab26dcc3d19741aece43ec48e58
wegfawefgawefg <668es218pur@gmail.com>
2026-06-13 01:18:07 +0000

    Validate arena allocator flow

--- scripts/test.sh
+++ scripts/test.sh
@@ -288,6 +288,7 @@ compile_and_expect type_aliases 42
 compile_and_expect enums 42
 compile_and_expect explicit_casts 42
 compile_and_expect allocation 17
+compile_and_expect arena_allocator 43
 compile_and_expect containers 42
 compile_and_expect cpp_template_interop 42; compile_and_expect cpp_move_unique_ptr 42; compile_and_expect cpp_filesystem_path 42; compile_and_expect cpp_chrono_timer 42
 compile_and_expect std_vector_map_string 42
