b5b80560d1d36941a36fb6ee510b276ef83d0cb3
wegfawefgawefg <668es218pur@gmail.com>
2026-06-19 23:10:35 +0000

    Advertise verbose project-driver options

--- src/dudu/cli_usage.cpp
+++ src/dudu/cli_usage.cpp
@@ -9,16 +9,16 @@ void print_cli_usage(bool project_driver) {
         std::cout << "usage: dudu init\n"
                      "       dudu init [path]\n"
                      "       dudu new <name>\n"
-                     "       dudu run [input.dd|target] [-o output] [--quiet]\n"
-                     "       dudu build [input.dd|target] [-o output] [--quiet]\n"
-                     "       dudu bench [compiler] [--quiet] [-- args...]\n"
+                     "       dudu run [input.dd|target] [-o output] [--quiet] [--verbose]\n"
+                     "       dudu build [input.dd|target] [-o output] [--quiet] [--verbose]\n"
+                     "       dudu bench [compiler] [--quiet] [--verbose] [-- args...]\n"
                      "       dudu check [input.dd|dir]\n"
                      "       dudu clean [path]\n"
                      "       dudu clean-cache [path]\n"
                      "       dudu fmt [input.dd|dir] [--check]\n"
                      "       dudu lsp\n"
                      "       dudu test [input.dd|target|filter] [--filter text] [--no-capture] "
-                     "[--quiet]\n"
+                     "[--quiet] [--verbose]\n"
                      "       dudu cmake [input.dd|target] [-o CMakeLists.txt]\n";
         return;
     }
@@ -35,8 +35,9 @@ void print_cli_usage(bool project_driver) {
            "[--no-capture]\n"
            "       duc fmt <input.dd|dir> [--check] [-o output.dd]\n"
            "       duc lsp\n"
-           "       duc run [input.dd|target] [-o output] [--quiet]\n"
-           "       duc test [input.dd|target|filter] [--filter text] [--no-capture] [--quiet]\n"
+           "       duc run [input.dd|target] [-o output] [--quiet] [--verbose]\n"
+           "       duc test [input.dd|target|filter] [--filter text] [--no-capture] [--quiet] "
+           "[--verbose]\n"
            "       duc <input.dd> [--check] [--format <path|->] "
            "[--emit-header <path|->] [--emit-c-header <path|->] "
            "[--emit-cpp <path|->] [-DNAME=value] [--verbose]\n";
